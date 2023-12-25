7a20d4be4b3f90312221b24a3178ce043dab81da
NamesMT <dangquoctrung123@gmail.com>
2023-12-25 19:53:46 +0700

    feat(base): adds whileWithTimeout

--- src/base.ts
+++ src/base.ts
@@ -1,6 +1,21 @@
+import type { Awaitable } from '.'
+import { isFunction, sleep } from '.'
+
 export {
   assert,
   toString,
   getTypeName,
   noop,
 } from '@antfu/utils'
+
+export async function whileWithTimeout(condition: Awaitable<boolean> | (() => Awaitable<boolean>), callback: () => void | Promise<void>, timeout = 3000) {
+  let timedout = false
+  sleep(timeout).then(() => timedout = true)
+
+  while (await (isFunction(condition) ? condition() : condition)) {
+    if (timedout)
+      throw new Error('whileWithTimeout: timed out')
+
+    await callback()
+  }
+}
