fbaa5e4fe7241ae27c4090156f7fba786afa011d
NamesMT <dangquoctrung123@gmail.com>
2023-12-26 09:29:48 +0700

    feat(promise): adds `uniquePromise`, `uniquify`

--- src/promise.ts
+++ src/promise.ts
@@ -1,3 +1,5 @@
+import type { FnWithArgs, Wrapped } from './types'
+
 export type {
   SingletonPromiseReturn,
   ControlledPromise,
@@ -61,4 +63,37 @@ export async function getPromiseState(promise: Promise<any>) {
     },
     reason => ({ state: 'rejected', reason }),
   )
+}
+
+const _uniquePromiseStore = {} as { [key: string]: Promise<any> }
+/**
+ * Execute a unique(singleton) instance of the promise,  
+ * multiple calls will all returns the same Promise while it haven't settled
+ * 
+ * Similar to `createSingletonPromise` but tracked by key instead of a wrapper scope
+ * 
+ * @param key - defaults to function name + args
+ * @param promiseCallback 
+ * @returns 
+ */
+export function uniquePromise(key: string, promiseCallback: FnWithArgs<Promise<any>>) {
+  if (typeof _uniquePromiseStore[key] !== 'undefined')
+    return _uniquePromiseStore[key]
+
+  return _uniquePromiseStore[key] = promiseCallback().finally(() => { delete _uniquePromiseStore[key] })
+}
+type Uniquified<T> = Wrapped<T>
+/**
+ * Wraps function with uniquePromise
+ *
+ * @param fn - Source function
+ * 
+ * @param key - Additionally set key prefix in case we have other functions with same name.
+ */
+export function uniquify<T extends Function>(fn: T, key?: string): Uniquified<T> {
+  const newFn = async function (this: any, ...args: any[]) {
+    return uniquePromise(key + fn.name + JSON.stringify(args), () => fn.apply(this, args))
+  } as any
+
+  return Object.defineProperty(newFn, 'name', { value: fn.name, writable: false })
 }
