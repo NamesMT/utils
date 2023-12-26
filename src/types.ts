75c8d44371e14fc139644f06cb9179f48f1d63ee
NamesMT <dangquoctrung123@gmail.com>
2023-12-26 09:10:03 +0700

    feat(types): adds Wrapped

--- src/types.ts
+++ src/types.ts
@@ -12,3 +12,7 @@ export type {
 } from '@antfu/utils'
 
 export type FnWithArgs<T = void> = (...args: any) => T
+
+export type Wrapped<T> = T extends (...args: infer A) => infer R
+  ? (...args: A) => R
+  : never
