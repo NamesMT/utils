04048551b994e6c5bf78fa9a455701d45e3103b8
NamesMT <dangquoctrung123@gmail.com>
2024-05-22 14:55:12 +0000

    feat: add `escapeRegex`, `variablePrefix`

--- src/base.ts
+++ src/base.ts
@@ -28,3 +28,40 @@ export async function whileWithTimeout(condition: Awaitable<boolean> | (() => Aw
     await callback()
   }
 }
+
+export function escapeRegex(str: string) {
+  return str.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&')
+}
+
+export type VariablePrefixed<I extends string | any[] | Record<string, any>, P extends string = '', S extends string = ''> =
+I extends string
+  ? `${P}${I}${S}`
+  : I extends any[]
+    ? { [E in keyof I]: VariablePrefixed<I[E] extends string | any[] | Record<string, any> ? I[E] : never, P, S> }
+    : I extends Record<string, any>
+      ? { [K in keyof I as `${P}${K & string}${S}`]: I[K] }
+      : never
+export function variablePrefix<I extends string | any[] | Record<string, any>, P extends string = '', S extends string = ''>(
+  variable: I,
+  prefix?: P,
+  suffix?: S,
+): VariablePrefixed<I, P, S> {
+  prefix = (prefix ?? '') as P
+  suffix = (suffix ?? '') as S
+
+  if (typeof variable === 'string') {
+    return `${prefix}${variable}${suffix}` as VariablePrefixed<I, P, S>
+  }
+  else if (Array.isArray(variable)) {
+    return variable.map(v => variablePrefix(v, prefix, suffix)) as VariablePrefixed<I, P, S>
+  }
+  else if (typeof variable === 'object' && variable !== null) {
+    const result: Record<string, any> = {}
+    Object.keys(variable).forEach((key) => {
+      result[`${prefix}${key}${suffix}`] = variable[key]
+    })
+    return result as VariablePrefixed<I, P, S>
+  }
+
+  throw new Error(`variable type not supported: ${typeof variable}`)
+}
