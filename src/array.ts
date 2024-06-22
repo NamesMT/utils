30953df28a5564d62c4767c915f2f28bb412611e
NamesMT <dangquoctrung123@gmail.com>
2024-06-22 09:06:11 +0000

    refactor(arrayFromIndexes): use `map` instead of loop

--- src/array.ts
+++ src/array.ts
@@ -2,8 +2,5 @@
  * Returns a new array with element at indexes of old array
  */
 export function arrayFromIndexes(arr: any[], idxs: number[]): any[] {
-  const _arr: typeof arr = []
-  idxs.forEach((idx, i) => { _arr[i] = arr[idx] })
-
-  return _arr
+  return idxs.map(idx => arr[idx])
 }
