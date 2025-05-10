653ac1c4df6dcd06692d3a0fc45073dc9d9a6d5f
NamesMT <dangquoctrung123@gmail.com>
2025-05-10 15:19:41 +0000

    feat: add `replaceByArray` fn

--- src/string.ts
+++ src/string.ts
@@ -35,3 +35,39 @@ export function replaceByMap(str: string, map: Record<string, string>, recursive
  * @deprecated Use {@link replaceByMap} instead
  */
 export const replaceMap = replaceByMap
+
+/**
+ * 
+ * @param recursive - If true, will retry all replacements until no more replacements are made
+ */
+export function replaceByArray(str: string, replaces: ReadonlyArray<[string | RegExp, string]>, recursive: boolean = false): string {
+  const builtRegExp: Record<string, RegExp> = {}
+  const getFindRE = (find: string | RegExp) => {
+    if (typeof find !== 'string')
+      return find
+
+    if (builtRegExp[find] === undefined)
+      builtRegExp[find] = new RegExp(escapeRegex(find), 'g')
+    
+    return builtRegExp[find]
+  }
+  
+  const doReplaces = () => {
+    for (const [find, replace] of replaces) {
+      const findRE = getFindRE(find)
+      str = str.replace(findRE, replace)
+    }
+  }
+
+  let mirror = str
+  doReplaces()
+
+  if (recursive) {
+    while (mirror !== str) {
+      mirror = str
+      doReplaces()
+    }
+  }
+
+  return str
+}
