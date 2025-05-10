9589aa87f89d0edb89917cdb76d6a8111fde599d
NamesMT <dangquoctrung123@gmail.com>
2025-05-10 15:15:12 +0000

    feat(replaceMap): rename to `replaceByMap`, add `recursive` arg

--- src/string.ts
+++ src/string.ts
@@ -1,10 +1,37 @@
 import { escapeRegex } from './base'
 
-export function replaceMap(string: string, map: Record<string, string>) {
-  for (const [find, replace] of Object.entries(map)) {
-    const findRE = new RegExp(escapeRegex(find), 'g')
-    string = string.replace(findRE, replace)
+/**
+ * Do replaces on a string using a map (Record)
+ * 
+ * @param recursive - If true, will retry all replacements until no more replacements are made
+ */
+export function replaceByMap(str: string, map: Record<string, string>, recursive: boolean = false): string {
+  const entries = Object.entries(map)
+  const builtRegExp: Record<string, RegExp> = {}
+  const doReplaces = () => {
+    for (const [find, replace] of entries) {
+      if (builtRegExp[find] === undefined)
+        builtRegExp[find] = new RegExp(escapeRegex(find), 'g')
+      
+      const findRE = builtRegExp[find]
+      str = str.replace(findRE, replace)
+    }
+  }
+  
+  let mirror = str
+  doReplaces()
+
+  if (recursive) {
+    while (mirror !== str) {
+      mirror = str
+      doReplaces()
+    }
   }
 
-  return string
+  return str
 }
+
+/**
+ * @deprecated Use {@link replaceByMap} instead
+ */
+export const replaceMap = replaceByMap
